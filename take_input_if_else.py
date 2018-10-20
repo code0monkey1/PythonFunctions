c6725da5cf3b3ddb2f2ed7fb9af4473e35511ba5
Chiranjeev Thomas <chiranjeevcollege@gmail.com>
2018-10-20 21:21:37 +0530

    Update take_input_if_else.py

---
+++ take_input_if_else.py
@@ -0,0 +1,15 @@
+print("What country are you in?...")
+country = input()
+if country == "Australia":
+    print("G'day")
+elif country == "New Zealand":
+    print("Kia ora")
+elif country == "Fiji":
+    print("Bula")
+elif country == "India":
+  print("namaste")
+else: 
+    print("Hello")
+
+
+    
