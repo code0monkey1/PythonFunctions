6a1ecf4d811b73bff56d3f35c8b5c513976e870c
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2018-11-22 16:03:33 +0530

    Added more functions

--- manipulating_strings.py
+++ manipulating_strings.py
@@ -12,3 +12,47 @@ input("Press ENTER to view the string's characters in reverse order")
 for ii in range(len(mystring)):
   print(mystring[-1-ii]) # Print the character at index (-1 minus ii) of mystring
   time.sleep(0.1)
+
+print("Chiru " * 3)
+# output : Chiru Chiru Chiru
+
+float(input("Enter a number: ")) + float(input("Enter another number: "))
+#Enter a number: 40
+#Enter another number: 2
+#42.0
+
+ x = 123.456
+ print(x)
+# 123.456
+ x = "This is a string"
+# print(x + "!")
+#This is a string!
+
+del variable_name # deletes the variable reference 
+
+#substring
+name = "chira"
+print(name[0:3])
+#chi
+
+print(len(name))
+#5
+
+#formatted string 
+statememt="hello my name is chiranjeev"
+l = f"{statememt} {statememt}" # this experession is evaluated at runtime
+print(l)
+
+#hello my name is chiranjeev hello my name is chiranjeev
+
+print(statememt.lower()) #these do exactly what you expect them to do 
+print(statememt.upper())
+print(statememt.title())
+print(statememt.strip())
+
+
+
+yess = "hello" not in statememt
+print(yess)
+
+# False 
