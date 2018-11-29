95206aebd17f6dbdead4a234c3ca5b64e327bfa8
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2018-11-29 22:48:44 +0530

    Update list_manipulation.py

--- list_manipulation.py
+++ list_manipulation.py
@@ -1,14 +1,15 @@
 #!/bin/python3
 # bucket.py - bucket list builder
 #
-print('\nBUCKET LIST BUILDER\n') # The \n creates a new line
-bucket = [] # Create an empty list called 'bucket'
-choice = input('Enter selection: e(X)it, (A)dd, (L)ist (R)emove...') # Ask for input before loop
-while choice != 'X': # This loop will continue until 'X' is entered
-if choice== 'A':
+print('\nBUCKET LIST BUILDER\n')  # The \n creates a new line
+bucket = []  # Create an empty list called 'bucket'
+# Ask for input before loop
+choice = input('Enter selection: e(X)it, (A)dd, (L)ist (R)emove...')
+while choice != 'X':  # This loop will continue until 'X' is entered
+if choice == 'A':
   print('Enter list item... ')
   newitem = input()
-        bucket.append(newitem) # Add 'newItem' to the list called 'bucket'
+        bucket.append(newitem)  # Add 'newItem' to the list called 'bucket'
       elif choice == 'L':
         for item in bucket: # This loop continues for however many items are in the list
         print(item)
@@ -27,26 +28,26 @@ print('\nGOODBYE!\n') # Only displayed if the loop has ended
  print(things[2])
  print(things[2][2])
 
-#Lists can be added and multiplied in the same way as strings.
-#For example:
+# Lists can be added and multiplied in the same way as strings.
+# For example:
 nums = [1, 2, 3]
 print(nums + [4, 5, 6]) #[1, 2, 3, 4, 5, 6]
 print(nums * 3) #[1, 2, 3, 1, 2, 3, 1, 2, 3]
 
-#To check if an item is in a list, the in operator can be used. It returns True if the item occurs one or more times in the list, and False if it doesn't.
+# To check if an item is in a list, the in operator can be used. It returns True if the item occurs one or more times in the list, and False if it doesn't.
 words = ["spam", "egg", "spam", "sausage"]
 print("spam" in words) #true
 print("egg" in words) #true
 print("tomato" in words) #false
 
-#The insert method is similar to append, except that it allows you to insert a new item at any position in the list, as opposed to just at the end.
+# The insert method is similar to append, except that it allows you to insert a new item at any position in the list, as opposed to just at the end.
 words = ["Python", "fun"]
 index = 1
 words.insert(index, "is")
 print(words) #['Python', 'is', 'fun']
 
-#The index method finds the first occurrence of a list item and returns its index.
-#If the item isn't in the list, it raises a ValueError.
+# The index method finds the first occurrence of a list item and returns its index.
+# If the item isn't in the list, it raises a ValueError.
 letters = ['p', 'q', 'r', 's', 'p', 'u']
 
 print(letters.index('r'))#2
@@ -59,26 +60,26 @@ list.count(obj) # Returns a count of how many times an item occurs in a list
 list.remove(obj) #Removes an object from a list
 list.reverse() # Reverses objects in a list
 
-#Range
+# Range
 
-#The range function creates a sequential list of numbers.
-#The code below generates a list containing all of the integers, up to 10.
+# The range function creates a sequential list of numbers.
+# The code below generates a list containing all of the integers, up to 10.
 numbers = list(range(10)) #The call to list is necessary because range by itself creates
 # a range object, and this must be converted to a list if you want to use it as one.
 print(numbers) #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-#If it is called with two arguments, it produces values from the first to the second.
-#For example:
+# If it is called with two arguments, it produces values from the first to the second.
+# For example:
 numbers = list(range(3, 8)) 
 print(numbers) #[3, 4, 5, 6, 7]
 print(range(20) == range(0, 20)) #True
 
-#range can have a third argument, which determines the interval of the sequence produced. 
-#This third argument must be an integer.
+# range can have a third argument, which determines the interval of the sequence produced. 
+# This third argument must be an integer.
 numbers = list(range(5, 20, 2))
 print(numbers) #[5, 7, 9, 11, 13, 15, 17, 19]
 
-#iterating through a list
+# iterating through a list
 words = ["hello", "world", "spam", "eggs"]
 for word in words:
   print(word + "!")
@@ -106,9 +107,9 @@ for word in words:
 
 Like the arguments to range, the first index provided in a slice is included in the result, but the second isn't.'''
 
-#If the first number in a slice is omitted, it is taken to be the start of the list.
-#If the second number is omitted, it is taken to be the end.
-#Example:
+# If the first number in a slice is omitted, it is taken to be the start of the list.
+# If the second number is omitted, it is taken to be the end.
+# Example:
 squares = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 print(squares[:7])
 print(squares[7:])
@@ -158,11 +159,11 @@ If a negative value is used for the step, the slice is done backwards.
 
 ***Using [::-1] as a slice is a common and idiomatic way to reverse a list.
 '''
-#What is the output of this code?
+# What is the output of this code?
 sqs = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 print(sqs[7:5:-1])
 
-#Ans : [49,36]
+# Ans : [49,36]
 
 '''List Comprehensions:
 List comprehensions are a useful way of quickly creating lists whose contents obey a simple rule.
@@ -173,9 +174,9 @@ cubes = [i**3 for i in range(5)]
 
 print(cubes)
 
-#Ans :>>>
-#[0, 1, 8, 27, 64]
-#>>>
+# Ans :>>>
+# [0, 1, 8, 27, 64]
+# >>>
 
 '''A list comprehension can also contain an if statement to enforce a condition on values in the list.
 Example:
@@ -191,9 +192,9 @@ Result:
 >>> 
 '''
 
-#Create a list of multiples of 3 from 0 to 20.
+# Create a list of multiples of 3 from 0 to 20.
 
-#Ans : a = [i for i in range(20) ifi%3==0]
+# Ans : a = [i for i in range(20) ifi%3==0]
 
 
 '''Trying to create a list in a very extensive range will result in a MemoryError.
@@ -206,7 +207,7 @@ MemoryError
 >>>
 '''
 
-#Fill in the blanks to create a list of numbers multiplied by 10 in the range of 5 to 9.
+# Fill in the blanks to create a list of numbers multiplied by 10 in the range of 5 to 9.
 
 a = [x*10 for x in range(5, 9)]
 
@@ -252,7 +253,7 @@ if any([i % 2 == 0 for i in nums]):
 
 for v in enumerate(nums):
    print(v)
-   Try It Yourself
+
 '''
 Result:
 >>>
