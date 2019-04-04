2b91e7114ddb175153ab98c5fd2d7c76c440939b
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2019-04-04 16:16:22 +0530

    Update math.py

--- math.py
+++ math.py
@@ -1,3 +1,4 @@
+import math
 # exponentiation ( ** )
  
 # print(2 ** 2) // result 4
@@ -7,3 +8,14 @@
 print(3/2) # with remiander
 
 print( 3 // 2) # without remainder 
+
+
+# abs() absolute value  and round() rounded number 
+
+print(round(-3.4))
+
+print(abs(-12)); 
+
+# advanced math operators that are accessible by using the math header library 
+print(math.ceil(12.2))  
+print(math.floor(12.9))
